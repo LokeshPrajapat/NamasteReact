faf157a2353bb6bdf7914b8931e48f5840d10671
Lokesh <lokeshprajapat742000@gmail.com>
2023-09-21 07:19:39 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,8 @@
+
+
+const parent=React.createElement("div",
+{},
+React.createElement("div",{},React.createElement("h1",{},"I am an h1 tag"))
+)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
